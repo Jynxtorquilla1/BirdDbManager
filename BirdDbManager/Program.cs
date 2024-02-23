a22810a3e829909d4b9be152e08298322e5b7e03
Jynxtorquilla1 <wojciech.szk@gmail.com>
2024-02-23 19:29:27 +0100

    User Interface

--- BirdDbManager/Program.cs
+++ BirdDbManager/Program.cs
@@ -1,49 +1,101 @@
 ﻿using BirdDbManager.Data;
 using BirdDbManager.Entities;
 using BirdDbManager.Repositories;
+using Microsoft.Extensions.Options;
+using System.Numerics;
 
+Console.WriteLine("Welcome to BirdBdManager!");
+Console.WriteLine("");
+Console.WriteLine("This app is aimed to help you storage and manage your ornithological data.");
+Console.WriteLine("");
 var speciesRepo = new SqlRepository<Species>(new BirdDbManagerDbContext());
+Starter();
 
-var observerRepo = new SqlRepository<Observer>(new BirdDbManagerDbContext());
-
-var areaRepo = new SqlRepository<StudyArea>(new BirdDbManagerDbContext());
-
-
-
-AddSpecies(speciesRepo);
-AddObserver(observerRepo);
-AddArea(areaRepo);
-PrintItemsFromRepository(speciesRepo);                                
-PrintItemsFromRepository<Observer>(observerRepo);
-PrintItemsFromRepository<StudyArea>(areaRepo);
-
-
-static void AddSpecies(IWriteRepository<Species> speciesRepo)
+void Starter()
 {
-    speciesRepo.Add(new Species { SpeciesName = "kaczka", number = 30 });
-    speciesRepo.Add(new Species { SpeciesName = "gęś", number = 12 });
-    speciesRepo.Add(new Species { SpeciesName = "indyk", number = 2 });
+    RunProgram();
+}
+
+void RunProgram()
+{
+    PrintMenu();
+    var menuInput = "";
+
+    while (menuInput != null)
+    {
+        menuInput = Console.ReadLine();
+
+        switch (menuInput.ToLower())
+        {
+            case "1":
+                AddNewBird();
+                break;
+            case "2":
+                RemoveExistingBird();
+                break;
+            case "3":
+                PrintData();
+                PrintItemsFromRepository(speciesRepo);
+                break;
+            case "4":
+                OpenLogFile();
+                break;
+            case "q":
+                break;
+            default:
+                Console.WriteLine("Please choose one of the options from menu");
+                continue;
+        }
+        break;
+    }
+    Console.WriteLine("Good job, see you next time!");
+
+}
+
+void OpenLogFile()
+{
+    throw new NotImplementedException();
+}
+
+void PrintData()
+{
+    throw new NotImplementedException();
+}
+
+void RemoveExistingBird()
+{
+    throw new NotImplementedException();
+}
+
+void AddNewBird()
+{
+    throw new NotImplementedException();
+}
+
+void PrintMenu()
+{
+    Console.WriteLine("Please choose one of the following options:");
+    Console.WriteLine("1 to add bird observation");
+    Console.WriteLine("2 to remove bird observation");
+    Console.WriteLine("3 to view all stored data");
+    Console.WriteLine("4 to open activity log file");
+    Console.WriteLine("Q to quit work");
+
+
+
+
+}
+
+
+PrintItemsFromRepository(speciesRepo);
+
+static void AddBird(IWriteRepository<Species> speciesRepo)
+{
     speciesRepo.Save();
 }
 
-static void AddObserver(IWriteRepository<Observer> observerRepo)
-{
-    observerRepo.Add(new Observer { FirstName = "Grażyna", LastName = "Gwiżdż" });
-    observerRepo.Add(new Observer { FirstName = "Janina", LastName = "Świszcz" });
-    observerRepo.Add(new Observer { FirstName = "Halina", LastName = "Brzęcz" });
-    observerRepo.Save();
-}
 
-static void AddArea(IWriteRepository<StudyArea> areaRepo)
-{
-    areaRepo.Add(new StudyArea { AreaName ="Zalesie", CentralPointLatitude = 51.716144M, CentralPointLongitude = 20.727396M });
-    areaRepo.Add(new StudyArea { AreaName = "Zapole", CentralPointLatitude = 51.974058M, CentralPointLongitude = 17.648338M });
-    areaRepo.Add(new StudyArea { AreaName = "Zamoście", CentralPointLatitude = 49.836884M, CentralPointLongitude = 20.613858M });
-    areaRepo.Save();
-} 
-
-
-static void PrintItemsFromRepository<T>(IReadRepository<T> repository) where T : class, IEntity               
+static void PrintItemsFromRepository<T>(IReadRepository<T> repository) where T : class, IEntity
 {
     var items = repository.GetAll();
 
